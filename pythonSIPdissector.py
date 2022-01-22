619166ad4b099427159c7fb68a3507831957e309
bbird81 <thomasguz@gmail.com>
2022-01-22 22:45:09 +0100

    inseriti dicts e aggiunti parsers

--- pythonSIPdissector.py
+++ pythonSIPdissector.py
@@ -7,42 +7,109 @@ class message:
         '''La porzione di messaggio che compone l'header SIP'''
         def __init__(self, headers_list = []):
             self.text = "\n".join(headers_list)
+            via_list = []
+            not_supported = []
+            self.via = via_list
+            self.not_supported = not_supported
+
             for line in headers_list:
                 #Parsing for From:
                 From_re = re.compile(r'From: (.*) <sip:(.*)@(.*)>(.*)')
-                From = From_re.search(line)
-                if From :
-                    self.From = From.group(0)
-                    self.From_label = From.group(1)
-                    self.From_user = From.group(2)
-                    self.From_realm = From.group(3)
+                FROM = From_re.match(line)
                 #Parsing for To:
-                To_re = re.compile(r'To: (.*) <sip:(.*)@(.*)>(.*)')
-                To = To_re.search(line)
-                if To :
-                    self.to = To.group(0)
-                    self.to_label = To.group(1)
-                    self.to_user = To.group(2)
-                    self.to_realm = To.group(3)
+                To_re = re.compile(r'To:(.*)<sip:(.*)@(.*)>(.*)')
+                TO = To_re.match(line)
                 #Parsing for Call-ID:
                 callId_re = re.compile(r'Call-ID: (.*)@(.*)')
-                CallID = callId_re.search(line)
-                if CallID :
-                    self.callId = CallID.group(0)
-                    self.callId_uuid = CallID.group(1)
-                    self.callId_realm = CallID.group(2)
+                CALLID = callId_re.match(line)
+                #Parsing for Via:
+                via_re = re.compile(r'Via: SIP/2.0/(TCP|UDP|TLS) (.*):(.*);branch=(.*)')
+                VIA = via_re.match(line)
+                #Parsing for CSeq:
+                cseq_re = re.compile(r'CSeq: (\d*) (INVITE|OPTIONS|BYE|CANCEL|ACK|PRACK|UPDATE|REFER|SUBSCRIBE|NOTIFY|INFO|REGISTER)')
+                CSEQ = cseq_re.match(line)
+                #Parsing for User-Agent
+                ua_re = re.compile(r'User-Agent: (.*)')
+                UA = ua_re.match(line)
+
+                if FROM :
+                    self.From = {'text': FROM.group(0),
+                                 'label': FROM.group(1).strip(),
+                                 'user': FROM.group(2),
+                                 'realm': FROM.group(3)}
+                    '''
+                    self.From = FROM.group(0)
+                    self.From_label = FROM.group(1).strip()
+                    self.From_user = FROM.group(2)
+                    self.From_realm = FROM.group(3)
+                    '''
+                elif TO :
+                    self.to = TO.group(0)
+                    self.to_label = TO.group(1)
+                    self.to_user = TO.group(2)
+                    self.to_realm = TO.group(3)
+                elif CALLID :
+                    self.callId = CALLID.group(0)
+                    self.callId_uuid = CALLID.group(1)
+                    self.callId_realm = CALLID.group(2)
+                elif VIA :
+                    self.via.append(VIA.group(0))
+                elif CSEQ :
+                    self.cseq = CSEQ.group(0)
+                    self.cseq_number = CSEQ.group(1)
+                    self.cseq_method = CSEQ.group(2)
+                elif UA:
+                    self.user_agent = UA.group(1)
+                else:
+                    not_supported.append(line)
+                ''' HEADERS RIMANENTI
+                Date: Thu, 20 Jan 2022 16:43:16 GMT
+                Supported: 100rel,timer,resource-priority,replaces,sdp-anat
+                Min-SE:  1800
+                Allow: INVITE, OPTIONS, BYE, CANCEL, ACK, PRACK, UPDATE, REFER, SUBSCRIBE, NOTIFY, INFO, REGISTER
+                Timestamp: 1642696996
+                Contact: <sip:1234324@1.1.1.1:5060>
+                Expires: 180
+                Allow-Events: telephone-event
+                Max-Forwards: 69
+                P-Asserted-Identity: "name" <sip:123456@1.1.1.1>
+                Session-ID: f8eb9a6e41395134832c9f66f9158bc2;remote=00000000000000000000000000000000
+                Session-Expires:  3600
+                Content-Type: application/sdp
+                Content-Disposition: session;handling=required
+                Content-Length: 307
+                '''
 
     class Body:
         '''La porzione di messaggio che compone il body SIP'''
         def __init__(self, body_list = []):
             self.text = "\n".join(body_list)
+            
+            a_list = []
+            m_list = []
+            self.a = a_list
+            self.m = m_list
+
             for line in body_list:
                 #Parsing for connection attribute:
                 c_re = re.compile(r'c=IN IP4 (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
                 C = c_re.search(line)
+                #Parsing for a= attribute:
+                a_re = re.compile(r'a=.*')
+                A = a_re.search(line)
+                #Parsing for media attribute:
+                m_re = re.compile(r'm=(\S.*) (\d{1,5}) RTP/AVP (\d.*)')
+                M = m_re.search(line)                
                 if C:
                     self.c = C.group(0)
                     self.c_ip = C.group(1)
+                elif A:
+                    a_list.append(A.group(0))
+                elif M:
+                    m_list.append({ 'text': M.group(0),
+                                    'type': M.group(1),
+                                    'port': M.group(2),
+                                    'codecs': M.group(3)})
 
     class ReqURI:
         '''La porzione di messaggio che compone la ReqURi'''
@@ -50,11 +117,17 @@ class message:
         def __init__(self, message):
             self.text = message
             requri_re = re.compile(r'(INVITE|OPTIONS|BYE|CANCEL|ACK|PRACK|UPDATE|REFER|SUBSCRIBE|NOTIFY|INFO|REGISTER) sip:(.*)@(.*):(\d+) SIP\/2\.0')
+            resuri_re = re.compile(r'SIP/2.0 (\d{3}) (.*)')
             REQURI = requri_re.search(message)
-            self.method = REQURI.group(1)
-            self.user = REQURI.group(2)
-            self.proxy = REQURI.group(3)
-            self.port = REQURI.group(4)
+            RESURI = resuri_re.search(message)
+            if REQURI:
+                self.method = REQURI.group(1)
+                self.user = REQURI.group(2)
+                self.proxy = REQURI.group(3)
+                self.port = REQURI.group(4)
+            if RESURI:
+                self.response_code = RESURI.group(1)
+                self.response_text = RESURI.group(2)
 
     def __init__(self, message):
         self.message = message #importo l'intero messaggio
